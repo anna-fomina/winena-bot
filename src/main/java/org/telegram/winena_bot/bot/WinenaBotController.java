dc887f5b7bcb98acf3f524a780a9b440b8f93202
anna <fomanjut@gmail.com>
2020-02-29 18:52:04 +0700

    fix circular view

--- src/main/java/org/telegram/winena_bot/bot/WinenaBotController.java
+++ src/main/java/org/telegram/winena_bot/bot/WinenaBotController.java
@@ -1,15 +1,15 @@
 package org.telegram.winena_bot.bot;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import javax.validation.constraints.NotEmpty;
 
-@Controller
+@RestController("/webhook")
 @RequiredArgsConstructor
 public class WinenaBotController {
     private final WinenaBot bot;

059acddea866d81dde934a65bf8c35e280c8e0d7
anna <fomanjut@gmail.com>
2020-02-29 18:58:01 +0700

    fix controller

--- src/main/java/org/telegram/winena_bot/bot/WinenaBotController.java
+++ src/main/java/org/telegram/winena_bot/bot/WinenaBotController.java
@@ -1,15 +1,13 @@
 package org.telegram.winena_bot.bot;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import javax.validation.constraints.NotEmpty;
 
-@RestController("/webhook")
+@RestController
+@RequestMapping("/webhook")
 @RequiredArgsConstructor
 public class WinenaBotController {
     private final WinenaBot bot;

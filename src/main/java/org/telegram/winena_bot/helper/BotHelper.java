2c0973d85dc2a7cd256695aa8eeb9e9a8152ec20
Anna Fomina <a.fomina@softservice.org>
2020-03-05 10:39:38 +0700

    change keyboard building

--- src/main/java/org/telegram/winena_bot/helper/BotHelper.java
+++ src/main/java/org/telegram/winena_bot/helper/BotHelper.java
@@ -1,5 +1,6 @@
 package org.telegram.winena_bot.helper;
 
+import com.google.common.collect.Lists;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
@@ -43,15 +44,15 @@ public class BotHelper {
     public static SendMessage getSendMessage(long chatId, String text, List<String> answers) {
         SendMessage s = getSendMessage(chatId, text);
 
-        var rowSize = answers.size() <=3 ? answers.size() : answers.size() / 3;
-        var buttons = new ArrayList<KeyboardRow>();
-        for (String answer : answers) {
-            KeyboardRow row = new KeyboardRow();
-            for (int j = 0; j < rowSize; j++) {
-                row.add(answer);
-            }
-            buttons.add(row);
-        }
+        var rowSize = (int) Math.ceil((double) answers.size() / 4);
+        var rows = Lists.partition(answers, rowSize);
+        var buttons = rows.stream().map(a -> {
+          KeyboardRow row = new KeyboardRow();
+          for (int i = 0; i < a.size(); i++) {
+            row.add(i, a.get(i));
+          }
+          return row;
+        }).collect(toList());
 
         s.setReplyMarkup(new ReplyKeyboardMarkup()
                 .setKeyboard(buttons)
